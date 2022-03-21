51fa4a4cf9f949dcea468d5e1c8b12e1b8f89bb4
Are Akash sai reddy <73512362+akash732@users.noreply.github.com>
2022-03-21 15:10:30 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,80 @@
+from queue import Queue
+
+class NQueens:
+    def __init__(self, size):
+        self.size = size
+        
+    def solve_dfs(self):
+        if self.size < 1:
+            return []
+        solutions = []
+        stack = [[]]
+        while stack:
+            solution = stack.pop()
+            if self.conflict(solution):
+                continue
+            row = len(solution)
+            if row == self.size:
+                solutions.append(solution)
+                continue
+            for col in range(self.size):
+                queen = (row, col)
+                queens = solution.copy()
+                queens.append(queen)
+                stack.append(queens)
+        return solutions
+
+    def solve_bfs(self):
+        if self.size < 1:
+            return []
+        solutions = []
+        queue = Queue()
+        queue.put([])
+        while not queue.empty():
+            solution = queue.get()
+            if self.conflict(solution):
+                continue
+            row = len(solution)
+            if row == self.size:
+                solutions.append(solution)
+                continue
+            for col in range(self.size):
+                queen = (row, col)
+                queens = solution.copy()
+                queens.append(queen)
+                queue.put(queens)
+        return solutions
+
+    def conflict(self, queens):
+        for i in range(1, len(queens)):
+            for j in range(0, i):
+                a, b = queens[i]
+                c, d = queens[j]
+                if a == c or b == d or abs(a - c) == abs(b - d):
+                    return True
+        return False
+
+    def print(self, queens):
+        for i in range(self.size):
+            print(' ---' * self.size)
+            for j in range(self.size):
+                p = 'Q' if (i, j) in queens else ' '
+                print('| %s ' % p, end='')
+            print('|')
+        print(' ---' * self.size)
+
+print('-->> N-Queens Problem <<--')
+size = int(input('Please enter the size of board: '))
+print_solutions = input('Do you want the solutions to be printed (Y/N): ').lower() == 'y'
+n_queens = NQueens(size)
+dfs_solutions = n_queens.solve_dfs()
+bfs_solutions = n_queens.solve_bfs()
+if print_solutions:
+    for i, solution in enumerate(dfs_solutions):
+        print('DFS Solution %d:' % (i + 1))
+        n_queens.print(solution)
+    for i, solution in enumerate(bfs_solutions):
+        print('BFS Solution %d:' % (i + 1))
+        n_queens.print(solution)
+print('Total DFS solutions: %d' % len(dfs_solutions))
+print('Total BFS solutions: %d' % len(bfs_solutions))
